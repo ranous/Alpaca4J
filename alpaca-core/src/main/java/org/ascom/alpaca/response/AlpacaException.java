be6a6a21b4f098ec3d020e685315de8f533b2943
Alex Ranous <github@ranous.com>
2025-02-11 12:56:53 -0800

    Implemented this, so don't need this anymore

--- alpaca-core/src/main/java/org/ascom/alpaca/response/AlpacaException.java
+++ alpaca-core/src/main/java/org/ascom/alpaca/response/AlpacaException.java
@@ -4,7 +4,6 @@ package org.ascom.alpaca.response;
 public class AlpacaException extends RuntimeException {
     private final int errorNumber;
     private long clientTransactionID;
-    // TODO: add serverTransactionID and stuff it in there when re-raising exceptions on the client side from the server response
 
     public AlpacaException(long clientTransactionID, int errorNumber) {
         super();
