c5efa2d4f0f777eede8a6c306094f42fd7d17dbb
Alex Ranous <github@ranous.com>
2025-02-14 00:38:10 -0800

    Let's handle these discovery responses in a threadpool for kicks and grins.  I don't anticipate ever needing to handle so many responses to a discovery broadcast that it's really needed, but why not?

--- alpaca-client-test/src/main/java/org/ascom/alpaca/client/ClientTest.java
+++ alpaca-client-test/src/main/java/org/ascom/alpaca/client/ClientTest.java
@@ -16,5 +16,6 @@ public class ClientTest {
         client.connect();
         double temp = client.getTemperature();
         System.out.println("Temperature is " + temp);
+        System.exit(0);
     }
 }
