f8c5c63f6b043e2c72893a948802709f38915b2e
Alex Ranous <github@ranous.com>
2025-02-18 02:04:02 -0800

    Remove the injected config from ServerInfo and into ManagementResource.  The injected config on ServerInfo doesn't work with Helidon.  Besides, ServerInfo should be a simple model object.

--- alpaca-core/src/main/java/org/ascom/alpaca/response/ServerInfo.java
+++ alpaca-core/src/main/java/org/ascom/alpaca/response/ServerInfo.java
@@ -1,22 +1,13 @@
 package org.ascom.alpaca.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 @SuppressWarnings("unused")
-@ApplicationScoped
 public class ServerInfo {
-    @Inject @ConfigProperty(name = "alpaca.server_info.server-name")
-    String serverName;
-    @Inject @ConfigProperty(name = "alpaca.server_info.manufacturer")
-    String manufacturer;
-    @Inject @ConfigProperty(name = "alpaca.server_info.manufacturer-version")
-    String manufacturerVersion;
-    @Inject @ConfigProperty(name = "alpaca.server_info.location")
-    String location;
+    private String serverName;
+    private String manufacturer;
+    private String manufacturerVersion;
+    private String location;
 
     public ServerInfo() {
 
--- alpaca-server/src/main/java/org/ascom/alpaca/webservices/ManagementResource.java
+++ alpaca-server/src/main/java/org/ascom/alpaca/webservices/ManagementResource.java
@@ -1,5 +1,10 @@
 package org.ascom.alpaca.webservices;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 import org.ascom.alpaca.device.DeviceManager;
 import org.ascom.alpaca.api.Management;
 import org.ascom.alpaca.device.Device;
@@ -8,29 +13,37 @@ import org.ascom.alpaca.response.*;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 
-@ApplicationScoped
+@Path("/management")
+@Produces(MediaType.APPLICATION_JSON)@ApplicationScoped
 public class ManagementResource implements Management {
     @Inject
     DeviceManager deviceManager;
-    private ServerInfo serverInfo = new ServerInfo();
-
-    @Inject
-    public void setServerInfo(ServerInfo serverInfo) {
-        this.serverInfo = serverInfo;
-    }
+    @Inject @ConfigProperty(name = "alpaca.server_info.server-name")
+    String serverName;
+    @Inject @ConfigProperty(name = "alpaca.server_info.manufacturer")
+    String manufacturer;
+    @Inject @ConfigProperty(name = "alpaca.server_info.manufacturer-version")
+    String manufacturerVersion;
+    @Inject @ConfigProperty(name = "alpaca.server_info.location")
+    String location;
 
     @Override
-    public ListResponse<Integer> getApiVersions(int clientID,
-                                                long clientTransactionID) {
+    @GET
+    @Path("apiversions")
+    public ListResponse<Integer> getApiVersions(@QueryParam("ClientID") int clientID,
+                                                @QueryParam("ClientTransactionID") long clientTransactionID) {
         return new ListResponse<>(clientTransactionID, List.of(1));
     }
 
     @Override
-    public ListResponse<DeviceDescriptor> getConfiguredDevices(int clientID,
-                                                               long clientTransactionID) {
+    @GET
+    @Path("v1/configureddevices")
+    public ListResponse<DeviceDescriptor> getConfiguredDevices(@QueryParam("ClientID") int clientID,
+                                                               @QueryParam("ClientTransactionID") long clientTransactionID) {
         ListResponse<DeviceDescriptor> response = new ListResponse<>(clientTransactionID);
         for (Device device : deviceManager.getDevices()) {
             response.addValue(device.getDeviceDescriptor());
@@ -38,9 +51,11 @@ public class ManagementResource implements Management {
         return response;
     }
 
-    public ServerInfoResponse getDescription(int clientID,
-                                             long clientTransactionID) {
+    @GET
+    @Path("v1/description")
+    public ServerInfoResponse getDescription(@QueryParam("ClientID") int clientID,
+                                             @QueryParam("ClientTransactionID") long clientTransactionID) {
 
-        return new ServerInfoResponse(clientTransactionID, serverInfo);
+        return new ServerInfoResponse(clientTransactionID, new ServerInfo(serverName, manufacturer, manufacturerVersion, location));
     }
 }
