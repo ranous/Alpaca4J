83fa50c4b300a5f4ed91d22d145b3e43461d55d5
Alex Ranous <github@ranous.com>
2025-02-24 19:34:56 -0800

    Reorganized into separate sync and async tests.  Also configure the logging.

--- alpaca-client-test/src/main/java/org/ascom/alpaca/client/ClientTest.java
+++ alpaca-client-test/src/main/java/org/ascom/alpaca/client/ClientTest.java
@@ -1,8 +1,21 @@
 package org.ascom.alpaca.client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.logging.LogManager;
+
 public class ClientTest {
     public static void main(String[] args) {
-        ClientManager manager = new ClientManager();
+        initLogger(ClientTest.class);
+        syncTest();
+        asyncTest();
+
+        System.exit(0);
+    }
+
+    static void syncTest() {
+        ClientManager manager = ClientManager.getInstance();
         System.out.println("Discovering devices...");
         manager.discoverDevices();
         System.out.println("Discovered " + manager.getClients().size() + " devices");
@@ -19,51 +32,84 @@ public class ClientTest {
         } else {
             System.out.println("Not safe to open the roof");
         }
+    }
 
-        // Let's call in async mode
-        client.isSafe(new AlpacaCallback<>() {
+    static void asyncTest() {
+        ClientManager manager = ClientManager.getInstance();
+        System.out.println("Discovering devices async...");
+        manager.discoverDevices(new AlpacaCallback<>() {
             @Override
-            public void success(Boolean result) {
-                if (result) {
-                    System.out.println("Async safe to open the roof");
-                } else {
-                    System.out.println("Async not safe to open the roof");
+            public void success(List<CommonClient> result) {
+                System.out.println("Discovered " + result.size() + " devices async");
+                checkRoof(manager);
+                // All done here, let the main thread know so we can exit
+                synchronized (manager) {
+                    manager.notify();
                 }
             }
 
             @Override
             public void error(AlpacaClientError error) {
-                System.err.println("Error checking safety: " + error.getErrorMessage());
+                System.err.println("Error discovering devices: " + error.getErrorMessage());
             }
         });
 
-        ObservingConditionsClient ocClient = manager.getClient(ObservingConditionsClient.class);
-        if (ocClient == null) {
-            System.err.println("Cannot find an observing conditions device");
+        // Wait for async calls to complete.
+        synchronized (manager) {
+            try {
+                manager.wait(20 * 1000);
+            } catch (InterruptedException e) {
+                System.err.println("Interrupted waiting for async calls to complete");
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Test Done");
+    }
+
+    private static void checkRoof(ClientManager manager) {
+        SafetyMonitorClient client = manager.getClient(SafetyMonitorClient.class);
+        if (client == null) {
+            System.err.println("Cannot find a safety monitor device");
             System.exit(1);
         }
-        ocClient.connect();
-        System.out.println("The temperature is " + ocClient.getTemperature());
-
-        ocClient.getName(new AlpacaCallback<>() {
+        // Need to connect to the device before we can use it
+        client.connect(new AlpacaCallback<>() {
             @Override
-            public void success(String result) {
-                System.out.println("The device name is " + result);
+            public void success(Void result) {
+                System.out.println("Connected to Safety Monitor");
+
+                client.isSafe(new AlpacaCallback<>() {
+                    @Override
+                    public void success(Boolean result) {
+                        if (result) {
+                            System.out.println("Async safe to open the roof");
+                        } else {
+                            System.out.println("Async not safe to open the roof");
+                        }
+                    }
+
+                    @Override
+                    public void error(AlpacaClientError error) {
+                        System.err.println("Error checking safety: " + error.getErrorMessage());
+                    }
+                });
             }
 
             @Override
             public void error(AlpacaClientError error) {
-                System.err.println("Error getting device name: " + error.getErrorMessage());
+                System.err.println("Error connecting to device: " + error.getErrorMessage());
+                System.exit(1);
             }
+
         });
-
-        // Give the async calls a chance to complete
+    }
+    private static void initLogger(Class classObject) {
+        // Attempt to read the logging configuration from the classpath
         try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            InputStream stream = classObject.getClassLoader().getResourceAsStream("logging.properties");
+            LogManager.getLogManager().readConfiguration(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        System.exit(0);
     }
 }
