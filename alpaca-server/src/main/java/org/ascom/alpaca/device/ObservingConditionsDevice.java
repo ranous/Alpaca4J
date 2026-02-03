9e39dbe6b7de9f7649c4e4126965323402baa120
Alex Ranous <github@ranous.com>
2026-02-03 15:02:04 -0800

    Add comprehensive Javadoc with ASCOM links for `ObservingConditionsDevice` methods
    - Enhanced documentation for all methods in `ObservingConditionsDevice`.
    - Included relevant ASCOM documentation links for detailed reference.

--- alpaca-server/src/main/java/org/ascom/alpaca/device/ObservingConditionsDevice.java
+++ alpaca-server/src/main/java/org/ascom/alpaca/device/ObservingConditionsDevice.java
@@ -16,34 +16,141 @@ public interface ObservingConditionsDevice extends Device {
     /**
      * Returns the time period in hours over which observations will be averaged.
      * @return the average period
-     * @throws org.ascom.alpaca.response.NotConnectedException if the client is not connected
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.AveragePeriod">A full description of this member's behavior is provided here</a>
      */
     double getAveragePeriod();
 
     /**
      * Sets the time period in hours over which observations will be averaged.
      * @param averagePeriod the average period specified, in hours.
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.AveragePeriod">A full description of this member's behavior is provided here</a>
      */
     void setAveragePeriod(double averagePeriod);
 
     /**
-     * Returns the cloud cover percentage.
-     * @return the cloud cover percentage
+     * Returns the amount of sky obscured by cloud
+     * @return the cloud cover in percent
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.CloudCover">A full description of this member's behavior is provided here</a>
      */
     double getCloudCover();
+
+    /**
+     * Returns the atmospheric dew point at the observatory
+     * @return the dew point in degrees Celsius
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.DewPoint">A full description of this member's behavior is provided here</a>
+     */
     double getDewPoint();
+
+    /**
+     * Returns the atmospheric humidity at the observatory
+     * @return the humidity in percent
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.Humidity">A full description of this member's behavior is provided here</a>
+     */
     double getHumidity();
+
+    /**
+     * Returns the atmospheric pressure at the observatory.
+     * @return the pressure in hPa
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.Pressure">A full description of this member's behavior is provided here</a>
+     */
     double getPressure();
+
+    /**
+     * Returns the rain rate at the observatory.
+     * @return the rain rate in mm/h
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.RainRate">A full description of this member's behavior is provided here</a>
+     */
     double getRainRate();
+
+    /**
+     * Returns the sky brightness at the observatory
+     * @return the sky brightness in lux
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.SkyBrightness">A full description of this member's behavior is provided here</a>
+     */
     double getSkyBrightness();
+
+    /**
+     * Returns the sky quality at the observatory.
+     * @return the sky quality in SQM
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.SkyQuality">A full description of this member's behavior is provided here</a>
+     */
     double getSkyQuality();
+
+    /**
+     * Returns the sky temperature at the observatory
+     * @return the sky temperature in degrees Celsius
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.SkyTemperature">A full description of this member's behavior is provided here</a>
+     */
     double getSkyTemperature();
+
+    /**
+     * Returns the seeing at the observatory
+     * @return the seeing in arcseconds
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.StarFWHM">A full description of this member's behavior is provided here</a>
+     */
     double getStarFWHM();
+
+    /**
+     * Returns the temperature at the observatory (°C)
+     * @return the temperature in degrees Celsius
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.Temperature">A full description of this member's behavior is provided here</a>
+     */
     double getTemperature();
+
+    /**
+     * Returns the wind direction at the observatory
+     * @return the wind direction in degrees
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.WindDirection">A full description of this member's behavior is provided here</a>
+     */
     double getWindDirection();
+
+    /**
+     * Returns the peak 3 second wind gust at the observatory over the last 2 minutes
+     * @return the wind gust in km/h
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.WindGust">A full description of this member's behavior is provided here</a>
+     */
     double getWindGust();
+
+    /**
+     * Returns the wind speed at the observatory.
+     * @return the wind speed in km/h
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.WindSpeed">A full description of this member's behavior is provided here</a>
+     */
     double getWindSpeed();
+
+    /**
+     * Refreshes sensor values from hardware.
+     *
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.Refresh">A full description of this member's behavior is provided here</a>
+     */
     void refresh();
+
+    /**
+     * Return a sensor description
+     *
+     * @param sensorName Sensor name
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.SensorDescription">A full description of this member's behavior is provided here</a>
+     */
     String getSensorDescription(String sensorName);
+
+    /**
+     * Return the time since the sensor value was last updated
+     *
+     * @param sensorName name of the sensor
+     * @see <a href="https://ascom-standards.org/newdocs/observingconditions.html#ObservingConditions.TimeSinceLastUpdate">A full description of this member's behavior is provided here</a>
+     */
     double getTimeSinceLastUpdate(String sensorName);
 }
